ef58a1e82095721ff78ecc35ca77786e6f6e0e6c
catav <catav.amnon@gmail.com>
2018-06-17 16:47:05 +0300

    parser for patchdock results

--- Utils/pdb_parser_wrapper.py
+++ Utils/pdb_parser_wrapper.py
@@ -6,22 +6,24 @@ class PDBParserWrapper():
     def __init__(self):
         self._parser = PDBParser()
 
+    def get_structure(self, id, file):
+        return self._parser.get_structure(id, file)
+
+    def get_result_complex_structure(self, receptor_pdb_id, ligand_pdb_id, rank):
+        receptor_path = file_paths.patchDock_result_receptor_path_format.format(receptor_pdb_id, ligand_pdb_id, rank)
+        receptor_struct = self.get_structure(receptor_pdb_id, receptor_path)
+        ligand_path = file_paths.patchDock_result_ligand_path_format.format(receptor_pdb_id, ligand_pdb_id, rank)
+        ligand_struct = self.get_structure(ligand_pdb_id, ligand_path)
+        return PatchDockResultComplexStructure(receptor_struct, ligand_struct)
+
 class PatchDockResultComplexStructure(object):
 
     def __init__(self, receptor_struct, ligand_struct):
         self.receptor_struct = receptor_struct
         self.ligand_struct = ligand_struct
 
-class PatchDockResultsParser(PDBParserWrapper):
-
-    def get_structure(self, receptor_pdb_id, ligand_pdb_id, rank):
-
-        pdb_path = file_paths.patchDock_result_path_format.format(receptor_pdb_id, ligand_pdb_id, rank)
-        pdb_id = "{}-{}-PatchDockResult{}".format(receptor_pdb_id, ligand_pdb_id, rank)
-        return self._parser.get_structure(pdb_id, pdb_path)
-
-class KnownComplexParser(PDBParserWrapper):
-
-    def get_structure(self, complex_pdb_id):
-        pdb_path = file_paths.pdb_file_path_format(complex_pdb_id)
-        return self._parser.get_structure(complex_pdb_id, pdb_path)
+    def get_atoms(self):
+        for atom in self.receptor_struct:
+            yield atom
+        for atom in self.ligand_struct:
+            yield atom
