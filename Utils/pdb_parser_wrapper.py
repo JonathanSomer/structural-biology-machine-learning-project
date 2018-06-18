94e1bb85e7ed0427626c07599b011a561dd9798a
catav <catav.amnon@gmail.com>
2018-06-18 12:29:50 +0300

    fix

--- Utils/pdb_parser_wrapper.py
+++ Utils/pdb_parser_wrapper.py
@@ -7,8 +7,9 @@ class PDBParserWrapper():
     def __init__(self):
         self._parser = PDBParser()
 
-    def get_structure(self, id, file):
-        return StructureWrapper(self._parser.get_structure(id, file))
+    def get_structure(self, id, pdb_id):
+        path = FilePaths.get_pdb_file_path(pdb_id)
+        return StructureWrapper(self._parser.get_structure(id, path))
 
     def get_result_complex_structure(self, receptor_pdb_id, ligand_pdb_id, rank):
         receptor_path = FilePaths.get_patch_dock_result_receptor_path(receptor_pdb_id, ligand_pdb_id, rank)
