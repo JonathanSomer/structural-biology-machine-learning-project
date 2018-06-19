e6d8352dc99e4fcf334e31c693dbec997f06ba92
shahar segal <shaharksegal@gmail.com>
2018-06-19 21:17:29 +0300

    unite pdb_utils with structure_utils

--- utils/pdb_utils.py
+++ utils/pdb_utils.py
@@ -1,4 +1,6 @@
+from Bio.PDB import PPBuilder
 from Bio.PDB.Residue import Residue
+from Bio.PDB.Structure import Structure
 
 
 def is_protein_residue(residue):
@@ -27,3 +29,14 @@ def get_residue_global_id(residue):
 
         global_id += len([r for r in chain.get_residues() if is_protein_residue(r)])
     return global_id
+
+
+def get_structure_sequence(struct):
+    # type: (Structure) -> str
+    """
+    Gets the structure sequence using PPBuilder
+    :param struct: Structure object
+    :return: struct sequence
+    """
+    ppb = PPBuilder()
+    return ''.join([pp.get_sequence() for pp in ppb.build_peptides(struct)])
--- utils/structure_utils.py
+++
@@ -1,9 +0,0 @@
-from Bio.PDB import PPBuilder
-
-
-def get_sequence(struct):
-    ppb = PPBuilder()
-    return ppb.build_peptides(struct)[0].get_sequence()
-
-def temp():
-    pass
