fca8b9cb0ea57bcc08dd86e1da349933cfd70cf7
catav <catav.amnon@gmail.com>
2018-08-20 15:17:20 +0300

    add resutls helper to pipeline

--- objects/pipeline_handler.py
+++ objects/pipeline_handler.py
@@ -2,8 +2,53 @@ from objects.complex import *
 from utils.capri_utils import get_capri_score
 from utils.fnat_utils import get_fnat_score
 
+class ResultsHelper(object):
 
-class PipelineHandler(object):
+    def __init__(self, complex_ids, n_of_patchdock_results, reranker):
+        self.n_of_patchdock_results = n_of_patchdock_results
+        self.reranker = reranker
+        self.complex_ids = complex_ids
+        self.ComplexHelpers = {}
+        for c_id in self.complex_ids:
+            self.ComplexHelpers[c_id] = ComplexHelper(c_id, self.n_of_patchdock_results, self.reranker)
+
+    def get_all_capri_scores_of_original_patchdock_ranking(self):
+        return [self.get_capri_score_of_original_patchdock_ranking(complex_id)
+                for complex_id in self.complex_ids]
+
+    def get_capri_score_of_original_patchdock_ranking(self, complex_id):
+        return self.ComplexHelpers[complex_id].get_capri_score_of_original_patchdock_ranking()
+
+    def get_all_capri_scores_of_reranking(self):
+        return [self.get_capri_score_of_reranking(complex_id)
+                for complex_id in self.complex_ids]
+
+    def get_capri_score_of_reranking(self, complex_id):
+        return self.ComplexHelpers[complex_id].get_capri_score_of_reranking()
+
+    def get_all_fnat_scores_before_reranking(self):
+        return [self.get_fnat_scores_before_reranking(complex_id)
+                for complex_id in self.complex_ids]
+
+    def get_fnat_scores_before_reranking(self, complex_id):
+        return self.ComplexHelpers[complex_id].get_fnat_scores_before_reranking()
+
+    def get_all_fnat_scores_after_reranking(self):
+        return [self.get_fnat_scores_after_reranking(complex_id)
+                for complex_id in self.complex_ids]
+
+    def get_fnat_scores_after_reranking(self, complex_id):
+        return self.ComplexHelpers[complex_id].get_fnat_scores_after_reranking()
+
+    def get_all_ranked_expectation_scores(self):
+        return [self.get_ranked_expectation_scores(complex_id)
+                for complex_id in self.complex_ids]
+
+    def get_ranked_expectation_scores(self, complex_id):
+        return self.ComplexHelpers[complex_id].get_ranked_expectation_scores()
+
+
+class ComplexHelper(object):
 
     def __init__(self, complex_id, n_of_patchdock_results, reranker):
         self.complex_id = complex_id
@@ -11,7 +56,6 @@ class PipelineHandler(object):
         self.reranker = reranker
         self.original_ranked_complexes = self._get_patchdock_results_by_original_rank(complex_id,
                                                                                       self.n_of_patchdock_results)
-
         self.unbound_complex = BenchmarkComplex(complex_id, ComplexType.zdock_benchmark_unbound)
         self.bound_complex = BenchmarkComplex(complex_id, ComplexType.zdock_benchmark_bound)
         self.reranked_complexes = reranker.rerank(self.original_ranked_complexes, detailed_return=False)
--- pipeline.py
+++ pipeline.py
@@ -1,26 +1,26 @@
 from Reranker.reranker import RaptorxReranker, RaptorXScoringMethod
-from objects.pipeline_handler import PipelineHandler
+from objects.pipeline_handler import ResultsHelper
 
-complex_id = '1BJ1'
-n_of_patchdock_results = 200
+complex_ids = ['1ACB', '1CGI']
+n_of_patchdock_results = 12
 reranker = RaptorxReranker(scoring_method=RaptorXScoringMethod.log_likelihood,
                            prob_trim=0.01)
 
-handler = PipelineHandler(complex_id, n_of_patchdock_results, reranker)
+helper = ResultsHelper(complex_ids, n_of_patchdock_results, reranker)
 
-original_score = handler.get_capri_score_of_original_patchdock_ranking()
-rerank_score = handler.get_capri_score_of_reranking()
+original_score = helper.get_all_capri_scores_of_original_patchdock_ranking()
+rerank_score = helper.get_all_capri_scores_of_reranking()
 
 print("original ranking capri score: {}".format(original_score))
 print("rerank capri score: {}".format(rerank_score))
 
 
-fnat_before_ranking = handler.get_fnat_scores_before_reranking()
-fnat_after_ranking = handler.get_fnat_scores_after_reranking()
+fnat_before_ranking = helper.get_all_fnat_scores_before_reranking()
+fnat_after_ranking = helper.get_all_fnat_scores_after_reranking()
 
 print("fnat scores before reranking: {}".format(fnat_before_ranking))
 print("fnat scores after reranking: {}".format(fnat_after_ranking))
 
-raptor_scores = handler.get_ranked_expectation_scores()
+raptor_scores = helper.get_all_ranked_expectation_scores()
 
 print("raptorx scores: {}".format(raptor_scores))
