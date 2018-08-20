b4678c3700c01bf92993ad83eaccacbef2ad2ba4
shahar segal <shaharksegal@gmail.com>
2018-08-20 13:05:48 +0300

    Adding cache sequences to complexes

--- objects/complex.py
+++ objects/complex.py
@@ -2,8 +2,13 @@ from Bio.PDB import NeighborSearch
 from abc import ABCMeta, abstractmethod
 from enum import Enum
 from Constants import *
-from utils.pdb_utils import pdb_parser, get_structure_sequence
+from utils.pdb_utils import pdb_parser
 import json
+
+import warnings
+from Bio.PDB.PDBExceptions import PDBConstructionWarning
+
+warnings.simplefilter('ignore', PDBConstructionWarning)
 
 NEIHGBOR_RADIUS = 5
 LIGAND_STRUCT_ID = 'ligand'
@@ -19,8 +24,13 @@ class ComplexType(Enum):
 class Complex(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self):
-        self._neighbours = None
+    def __init__(self, complex_id):
+        self._complex_id = complex_id
+        if not self._is_cached():
+            self._cache_complex()
+        cache = self._get_complex_cache()
+        self._receptor_sequence, self._ligand_sequence = cache['r_seq'], cache['l_seq']
+        self._neighbours = [tuple(nb) for nb in cache['nb5']]
 
     @property
     def complex_id(self):
@@ -32,15 +42,39 @@ class Complex(object):
 
     @property
     def receptor(self):
-        return self._receptor
+        return self._lazy_init('_receptor', self._init_complex)
 
     @property
     def ligand(self):
-        return self._ligand
+        return self._lazy_init('_ligand', self._init_complex)
+
+    @property
+    def receptor_sequence(self):
+        return self._receptor_sequence
+
+    @property
+    def ligand_sequence(self):
+        return self._ligand_sequence
+
+    @staticmethod
+    def get_structure_sequence(struct):
+        # type: (Structure) -> str
+        """
+        Gets the structure sequence using PPBuilder
+        :param struct: Structure object
+        :return: struct sequence
+        """
+        ppb = PPBuilder()
+        return ''.join([str(pp.get_sequence()) for pp in ppb.build_peptides(struct)])
 
     @abstractmethod
     def _init_complex(self):
         pass
+
+    def _lazy_init(self, attr_str, init_fn):
+        if not hasattr(self, attr_str) or getattr(self, attr_str, None) is None:
+            init_fn(self)
+        return getattr(self, attr_str)
 
     def _add_true_residue_indexes(self):
         '''
@@ -91,8 +125,8 @@ class Complex(object):
         c_path = self._get_cache_path()
         cache = {
             "complex_id": self.complex_id,
-            "l_seq": get_structure_sequence(self.ligand),
-            "r_seq": get_structure_sequence(self.receptor),
+            "l_seq": Complex.get_structure_sequence(self.ligand),
+            "r_seq": Complex.get_structure_sequence(self.receptor),
             "nb5": self.get_neighbours_residues()
         }
         with open(c_path, 'w') as f:
@@ -109,18 +143,13 @@ class Complex(object):
     def _get_cache_path(self):
         raise NotImplementedError("abs method")
 
+
 class BenchmarkComplex(Complex):
 
     def __init__(self, complex_id, type=ComplexType.zdock_benchmark_bound):
-        super(BenchmarkComplex, self).__init__()
+        super(BenchmarkComplex, self).__init__(complex_id)
         self._type = type
-        self._complex_id = complex_id
-        self._ligand, self._receptor = self._init_complex()
         self._add_true_residue_indexes()
-        if not self._is_cached():
-            self._cache_complex()
-        cache = self._get_complex_cache()
-        self._neighbours = [tuple(nb) for nb in cache['nb5']]
 
     def _init_complex(self):
         bound = self.type == ComplexType.zdock_benchmark_bound
@@ -128,50 +157,45 @@ class BenchmarkComplex(Complex):
         receptor_pdb_file_path = get_zdock_benchmark_pdb_path(self.complex_id, ligand=False, bound=bound)
         ligand = pdb_parser.get_structure(LIGAND_STRUCT_ID, ligand_pdb_file_path)
         receptor = pdb_parser.get_structure(RECEPTOR_STRUCT_ID, receptor_pdb_file_path)
-        return ligand, receptor
+        self._ligand, self._receptor = ligand, receptor
 
     def _get_cache_path(self):
         bound = self.type == ComplexType.zdock_benchmark_bound
         return get_zdock_benchmark_cache_path(self.complex_id, bound)
 
+
 class PatchDockComplex(Complex):
 
     def __init__(self, complex_id, rank):
-        super(PatchDockComplex, self).__init__()
-        self._complex_id = complex_id
+        super(PatchDockComplex, self).__init__(complex_id)
         self._type = ComplexType.patch_dock
         self.original_rank = rank
-        self._ligand_chain_ids, self._receptor_chain_ids = self._infer_r_l_chain_ids_from_banchmark_complex()
-        self._ligand, self._receptor = self._init_complex()
         self._add_true_residue_indexes()
-        if not self._is_cached():
-            self._cache_complex()
-        cache = self._get_complex_cache()
-        self._neighbours = [tuple(nb) for nb in cache['nb5']]
-
-    def _infer_r_l_chain_ids_from_banchmark_complex(self):
-        benchmark_complex = BenchmarkComplex(self._complex_id, type=ComplexType.zdock_benchmark_unbound)
-        receptor_chain_ids = [chain.get_id() for chain in list(benchmark_complex.receptor.get_chains())]
-        ligand_chain_ids = [chain.get_id() for chain in list(benchmark_complex.ligand.get_chains())]
-
-        return ligand_chain_ids, receptor_chain_ids
 
     def _init_complex(self):
+        # type: () -> None
+        def _infer_r_l_chain_ids_from_benchmark_complex(self):
+            benchmark_complex = BenchmarkComplex(self._complex_id, type=ComplexType.zdock_benchmark_unbound)
+            receptor_chain_ids = [chain.get_id() for chain in list(benchmark_complex.receptor.get_chains())]
+            ligand_chain_ids = [chain.get_id() for chain in list(benchmark_complex.ligand.get_chains())]
+            return ligand_chain_ids, receptor_chain_ids
+
+        def _remove_chain_from_struct(struct, chain_id):
+            first_model = struct.get_list()[0]  # struct children are models
+            first_model.detach_child(chain_id)  # model children are chains
+
+        _ligand_chain_ids, _receptor_chain_ids = _infer_r_l_chain_ids_from_benchmark_complex(self)
         patch_dock_complex_path = get_patchdock_ranked_complex_pdb_path(self._complex_id, self.original_rank)
         ligand = pdb_parser.get_structure(LIGAND_STRUCT_ID, patch_dock_complex_path)
         receptor = pdb_parser.get_structure(RECEPTOR_STRUCT_ID, patch_dock_complex_path)
 
         for chain_id in self._receptor_chain_ids:
-            self._remove_chain_from_struct(ligand, chain_id)
+            _remove_chain_from_struct(ligand, chain_id)
 
         for chain_id in self._ligand_chain_ids:
-            self._remove_chain_from_struct(receptor, chain_id)
+            _remove_chain_from_struct(receptor, chain_id)
 
-        return ligand, receptor
-
-    def _remove_chain_from_struct(self, struct, chain_id):
-        first_model = struct.get_list()[0]  # struct children are models
-        first_model.detach_child(chain_id)  # model children are chains
+        self._ligand, self._receptor = ligand, receptor
 
     def _get_cache_path(self):
         return get_patchdock_ranked_complex_cache_path(self.complex_id, self.original_rank)
