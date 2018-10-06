7717857f531e917f4ddf9c92a418044b451727af
catav <catav.amnon@gmail.com>
2018-10-06 21:22:51 +0300

    add radius as parameter for results helper

--- objects/processed_result.py
+++ objects/processed_result.py
@@ -3,11 +3,15 @@ from Constants import NEIGHBOUR_RADII, NEIGHBOUR_ATTR_TEMPLATE, NEIGHBOUR_DEFAUL
 
 class ComplexProcessedResult(object):
 
-    def __init__(self, complex_id, receptor_sequence, ligand_sequence, attr_dict):
+    def __init__(self, complex_id, receptor_sequence, ligand_sequence, attr_dict,
+                 neighbor_radius=NEIGHBOUR_DEFAULT_RADIUS):
         self.__dict__ = {"_" + k: v for k, v in attr_dict.items()}
         self._complex_id = complex_id
         self._receptor_sequence = receptor_sequence
         self._ligand_sequence = ligand_sequence
+        if neighbor_radius not in NEIGHBOUR_RADII:
+            raise ValueError("radius value is invalid")
+        self.neighbor_radius = neighbor_radius
 
     @property
     def complex_id(self):
@@ -21,26 +25,24 @@ class ComplexProcessedResult(object):
     def ligand_sequence(self):
         return self._ligand_sequence
 
-    def get_neighbours_residues(self, neighbor_radius=NEIGHBOUR_DEFAULT_RADIUS):
-        # type: (int) -> List[Tuple[int, int]]
+    def get_neighbours_residues(self):
+        # type: () -> List[Tuple[int, int]]
         """
         :param: neighbor_radius - neighbour radius to use
         :return: list of tuples (receptor_residue_index, ligand_residue_index) in which the euclidean distance
                  between them is at most $(NEIGHBOURS_RADIUS)
         """
-        return self._get_attr_by_radius(NEIGHBOUR_ATTR_TEMPLATE, neighbor_radius)
+        return self._get_attr_by_template(NEIGHBOUR_ATTR_TEMPLATE)
 
-    def get_fnat_score(self, neighbor_radius=NEIGHBOUR_DEFAULT_RADIUS):
-        # type: (int) -> int
+    def get_fnat_score(self):
+        # type: () -> int
         """
         Returns the fnat score for the given radius
         :param: neighbor_radius - neighbour radius to use
         :return: fnat score
         """
-        return self._get_attr_by_radius(FNAT_ATTR_TEMPLATE, neighbor_radius)
+        return self._get_attr_by_template(FNAT_ATTR_TEMPLATE)
 
-    def _get_attr_by_radius(self, attr_template, neighbor_radius):
-        if neighbor_radius not in NEIGHBOUR_RADII:
-            raise ValueError("radius value is invalid")
-        rad_attr = attr_template % neighbor_radius
+    def _get_attr_by_template(self, attr_template):
+        rad_attr = attr_template % self.neighbor_radius
         return getattr(self, rad_attr)
--- objects/results_helper.py
+++ objects/results_helper.py
@@ -2,11 +2,11 @@ import numpy as np
 import utils.capri_utils as capri_utils
 from objects.complex import *
 from objects.processed_result import ComplexProcessedResult
-
+from Constants import NEIGHBOUR_DEFAULT_RADIUS
 
 class ResultsHelper(object):
 
-    def __init__(self, complex_ids, n_of_patchdock_results, reranker,
+    def __init__(self, complex_ids, n_of_patchdock_results, reranker, nb_radius=NEIGHBOUR_DEFAULT_RADIUS,
                  ignore_failure=False, verbose=True):
         self.n_of_patchdock_results = n_of_patchdock_results
         self.reranker = reranker
@@ -15,7 +15,7 @@ class ResultsHelper(object):
             if verbose:
                 print("ResultHelper Loading complex_id: %s" % c_id)
             try:
-                self.complex_helpers[c_id] = ComplexHelper(c_id, self.n_of_patchdock_results, self.reranker)
+                self.complex_helpers[c_id] = ComplexHelper(c_id, self.n_of_patchdock_results, self.reranker, nb_radius)
             except Exception as e:
                 print("%s: %s" % (c_id, str(e)))
                 if not ignore_failure:
@@ -65,10 +65,11 @@ class ResultsHelper(object):
 
 class ComplexHelper(object):
 
-    def __init__(self, complex_id, n_of_patchdock_results, reranker):
+    def __init__(self, complex_id, n_of_patchdock_results, reranker, nb_radius):
         self.complex_id = complex_id
         self.n_of_patchdock_results = n_of_patchdock_results
         self.reranker = reranker
+        self.nb_radius = nb_radius
         self.receptor_sequence = self.get_sequence_from_fasta(ligand=False)
         self.ligand_sequence = self.get_sequence_from_fasta(ligand=True)
         self.original_ranked_complexes = self._get_patchdock_results()
@@ -76,10 +77,11 @@ class ComplexHelper(object):
         self.reranked_complexes = [t[0] for t in self.detailed_reranked_complexes]
 
     def _get_patchdock_results(self):
-        # type: (str, int) -> List[ComplexProcessedResult]
+        # type: () -> List[ComplexProcessedResult]
         with open(get_processed_data_json_path(self.complex_id)) as f:
             results_json = json.load(f)
-        return [ComplexProcessedResult(self.complex_id, self.receptor_sequence, self.ligand_sequence, results_json[i])
+        return [ComplexProcessedResult(self.complex_id, self.receptor_sequence,
+                                       self.ligand_sequence, results_json[i], self.nb_radius)
                 for i in range(self.n_of_patchdock_results)]
 
     def get_sequence_from_fasta(self, ligand):
